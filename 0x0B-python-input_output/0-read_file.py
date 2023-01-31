8b2c018c0149fb15b3d6e7e7f1d3ce5db7609694
JasonBeagle <skipskipperson385@gmail.com>
2023-01-31 20:31:42 +0000

    proj0x08-py.input/output-task-1,correction

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,6 @@
 
 def read_file(filename=""):
     """reading file"""
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, mode='r', encoding='utf-8') as f:
         read_words = f.read()
         print(read_words)
