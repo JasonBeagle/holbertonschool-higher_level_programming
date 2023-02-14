dcc930354413b3d5369e1bed2226c1993058a848
JasonBeagle <skipskipperson385@gmail.com>
2023-02-14 15:28:18 -0800

    proj0x00-SQL,task,10,11,12,13

---
+++ 0x00-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- updates Bob's score to 10 in the table second_table
+-- updates a value in a table
+UPDATE `second_table` SET `score` = 10 WHERE `name` = 'Bob';
---
+++ 0x00-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- removes scores <= 5 in second_table of the database hbtn_0c_0 in your MySQL server
+-- removes all records that fit a certain condition
+DELETE FROM second_table WHERE score <= 5;
