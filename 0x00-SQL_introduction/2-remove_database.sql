f5b65cd91193fce39825818fc29fa4a3193dd0de
JasonBeagle <skipskipperson385@gmail.com>
2023-02-14 13:54:46 -0800

    proj0x00-SQL,task2,3,4

---
+++ 0x00-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- removes database, htbn_0c_0, from MySQL server
+-- script shouldn't fail if already exists
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ 0x00-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- lists tables of a database
+-- database name passes as arg of mysql command
+SHOW TABLES;
