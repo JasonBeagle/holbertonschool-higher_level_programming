f3cec38c52412837f18461b222fc54dcfc6d324a
JasonBeagle <skipskipperson385@gmail.com>
2023-03-01 07:57:54 -0800

    correction,task3

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""injection safe method to lists items within database hbtn_0e_0_usa 
+"""injection safe method to lists items within database hbtn_0e_0_usa
     based off given argument"""
 
 import MySQLdb
@@ -8,7 +8,7 @@ import sys
 if __name__ == '__main__':
 
     db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name=%s\
