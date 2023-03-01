fd87348836432dcc82d9450d381a4be500bd666b
JasonBeagle <skipskipperson385@gmail.com>
2023-03-01 09:12:27 -0800

    task14,correction

--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -7,6 +7,8 @@ Base = declarative_base()
 
 
 class City(Base):
+    """class City
+        attributes id, name, and state_id"""
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True, nullable=False,
                 autoincrement=True, unique=True)
