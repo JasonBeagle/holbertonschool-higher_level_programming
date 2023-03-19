f1411425d4983805c8f2108812100a8979802ecf
JasonBeagle <skipskipperson385@gmail.com>
2023-03-19 20:57:25 +0000

    task4,correction

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -9,7 +9,6 @@ import requests
 if __name__ == "__main__":
     response = requests.get('https://intranet.hbtn.io/status')
 
-
     print("Body response:")
     print("\t- type: {}".format(type(response.text)))
     print("\t- content: {}".format(response.text))
